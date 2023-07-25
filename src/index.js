dcc067789c69a047cdcf50d4c226f74f65bf2d3d
Bayram Emiraliev <emiraliev.kad@gmail.com>
2023-07-26 00:13:08 +0300

    Сверстал блок Портфолио

--- src/index.js
+++ src/index.js
@@ -4,25 +4,25 @@ const prevBtn = document.querySelector('.prev_btn');
 
 const btnPortfolio = document.querySelectorAll('.slider-container > button');
 
-let idx = 0;
-imgPortfolio[idx].classList.add('active');
+// let idx = 0;
+// imgPortfolio[idx].classList.add('active');
 
-const handleClickBtn = (event) => {
-  let target = event.target;
+// const handleClickBtn = (event) => {
+//   let target = event.target;
 
-  imgPortfolio.forEach((img) => {
-    img.classList.remove('active');
-  });
-  if (target.closest(`.next_btn`)?.className === 'next_btn') {
-    idx++;
-    if (idx === imgPortfolio.length) idx = 0;
-  } else if (target.closest(`.prev_btn`)?.className === 'prev_btn') {
-    idx--;
-    if (idx < 0) idx = imgPortfolio.length - 1;
-  }
-  imgPortfolio[idx].classList.add('active');
-};
+//   imgPortfolio.forEach((img) => {
+//     img.classList.remove('active');
+//   });
+//   if (target.closest(`.next_btn`)?.className === 'next_btn') {
+//     idx++;
+//     if (idx === imgPortfolio.length) idx = 0;
+//   } else if (target.closest(`.prev_btn`)?.className === 'prev_btn') {
+//     idx--;
+//     if (idx < 0) idx = imgPortfolio.length - 1;
+//   }
+//   imgPortfolio[idx].classList.add('active');
+// };
 
-btnPortfolio.forEach((btn) => {
-  btn.addEventListener('click', handleClickBtn);
-});
+// btnPortfolio.forEach((btn) => {
+//   btn.addEventListener('click', handleClickBtn);
+// });
